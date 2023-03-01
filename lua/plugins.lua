af1e3d5a88b2b77318e7e906f59a1282833d2be6
kkard2 <kkard2.acct@gmail.com>
2023-03-01 14:45:11 +0100

    feat: add lualine

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,4 +6,11 @@ return require('packer').startup(function(use)
     flavour = 'mocha',
     no_italic = true,
   })
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    as = 'lualine',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  require('lualine').setup()
 end)
