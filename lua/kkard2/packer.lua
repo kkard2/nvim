616d30bff05afa589ee622676a436884b0388407
kkard2 <kkard2.acct@gmail.com>
2023-04-13 09:55:43 +0200

    feat: add copilot

--- lua/kkard2/packer.lua
+++ lua/kkard2/packer.lua
@@ -41,5 +41,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+
+    use 'zbirenbaum/copilot.lua'
 end)
 
