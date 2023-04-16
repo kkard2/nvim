9b48e7dacd792548d03ee2e19d376f3f51766191
kkard2 <kkard2.acct@gmail.com>
2023-04-16 10:49:46 +0200

    feat: change shell to powershell on windows

--- lua/kkard2/set.lua
+++ lua/kkard2/set.lua
@@ -27,3 +27,7 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 vim.g.mapleader = " "
 
+-- only change shell to ps if on windows
+if vim.fn.has("win32") then
+    vim.opt.shell = "powershell"
+end
